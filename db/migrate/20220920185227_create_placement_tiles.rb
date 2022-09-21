649690e355f11b464568a2a4d1f16231df0016fc
Elizabeth Nguyen <saper209@yahoo.com>
2022-09-21 11:34:42 -0700

    Adjust position for placement tile

--- db/migrate/20220920185227_create_placement_tiles.rb
+++ db/migrate/20220920185227_create_placement_tiles.rb
@@ -2,7 +2,8 @@ class CreatePlacementTiles < ActiveRecord::Migration[6.1]
   def change
     create_table :placement_tiles do |t|
       t.integer :size
-      t.array :position
+      t.integer :positionx
+      t.integer :positiony
       t.boolean :occupied
 
       t.timestamps
