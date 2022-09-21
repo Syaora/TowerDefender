c9c9749613d84f6246fcdb57474b9a1f32e3232c
Elizabeth Nguyen <saper209@yahoo.com>
2022-09-21 03:11:31 -0700

    Added UserGames controller and routes

---
+++ app/controllers/user_games_controller.rb
@@ -0,0 +1,49 @@
+class UserGamesController < ApplicationController
+  def index
+    user_games = UserGame.all 
+    render json: user_games
+  end
+
+  def create
+    user_game = UserGame.create(user_game_params)
+    render json: user_game, status: :created
+  end
+
+  def show
+    user_game = UserGame.find_by(id: params[:id])
+    if user_game
+      render json: user_game
+    else
+      render_not_found_response
+    end
+  end
+
+  def update
+    user_game = UserGame.find_by(id: params[:id])
+    if user_game
+      user_game.update(user_game)
+      render json: user_game 
+    else
+      render_not_found_response
+    end
+  end
+
+  def destroy
+    user_game = UserGame.find_by(id: params[:id])
+    if user_game
+      user_game.destroy
+      head :no_content
+    else
+      render_not_found_response
+    end
+  end
+
+  private
+
+  def user_game_params
+    params.permit(:user_id, :game_id, :nmame, :money, :heatlh, :round_position)
+
+  def render_not_found_response
+    render json: { error: "Game not found" }, status: :not_found
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create] do
+    resources :usergames 
+  end
 
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
