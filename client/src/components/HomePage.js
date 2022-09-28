a2db72d5e042668c0e3052ec61f998dcda0e6957
Elizabeth Nguyen <saper209@yahoo.com>
2022-09-28 01:04:09 -0700

    Fix Login

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,1 +1,36 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import { Link } from "react-router-dom"
 
+export default function HomePage() {
+  return (
+    <Stack
+    sx={{ pt: 4 }}
+    direction="row"
+    spacing={2}
+    justifyContent="center"
+  >
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea component={Link} to="/login">
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Login
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea component={Link} to="/signup">
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Sign Up
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  </Stack>
+  )
+}
