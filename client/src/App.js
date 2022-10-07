a0778af637923f8e7c5b8ec6b2290f8b9ef7f469
Elizabeth Nguyen <saper209@yahoo.com>
2022-10-06 22:30:26 -0700

    Redo client routes

--- client/src/App.js
+++ client/src/App.js
@@ -25,16 +25,10 @@ function App() {
             <Route exact path="/" element={<HomePage />} />
             <Route path="/register" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }/>
-            <Route path="/game" element={
-              <PrivateRoute>
-                <Game />
-              </PrivateRoute>
-            }/>
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/game" element={<Game />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </UserProvider>
